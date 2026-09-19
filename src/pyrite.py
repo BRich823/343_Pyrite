1d2aa3efdb71f106e9e0c003b1f776ab41212e3c
Brody <brodyrichards23@gmail.com>
2026-09-18 21:36:17 -0400

    readability change
    added exit message and usage message

--- src/pyrite.py
+++ src/pyrite.py
@@ -9,24 +9,17 @@ def main():
 		while True:
 			try:
 				_ = input()
-			except EOFError:
+			except KeyboardInterrupt:
+				print("Exiting REPL")
 				break
 			print("Scanner Not Implemented")
 
 	elif len(args) == 1:
-		# print the contents of the provided file
-		try:
-			with open(args[0], "r", encoding="utf-8") as f:
-				# print file contents without adding extra text
-				print(f.read(), end="")
-		except FileNotFoundError:
-			print(f"File not found: {args[0]}")
-			sys.exit(1)
-
+		print("Scanner Not Implemented")
+		with open(args[0]) as f:
+			print(f.read())
 	else:
-		# two or more arguments -> not supported
-		print("Unexpected number of arguments")
-		sys.exit(2)
+		print("Usage: python src/pyrite.py or python src/pyrite.py [file]")
 
 
 if __name__ == "__main__":
